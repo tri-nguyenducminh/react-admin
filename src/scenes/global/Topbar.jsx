f71d8a2b6a373c1547af1ffab65581325cac77c2
tri-nguyenducminh <tri.nguyenducminh@ncc.asia>
2022-12-20 17:49:56 +0700

    update topbar 20/12 47:25

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,13 +1,39 @@
 import {Box, IconButton, useTheme} from "@mui/material"
-import { useContext } from "react";
-import { ColorModeContext, token } from "../../theme";
+import {useContext} from "react";
+import {ColorModeContext, tokens} from "../../theme";
 import InputBase from "@mui/material";
-import { LightModeOutlinedIcon } from "@mui/icons-material/LightModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutLined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutLined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutLined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search"
+import styled from "@emotion/styled";
+
+const styledBox = styled(Box);
 
 const Topbar = () => {
-    return <div>
-        Topbar
-    </div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+
+    return (<Box display="flex" justifyContent="space-between" p={2}>
+        {/*Search Bar*/}
+        <Box
+            display="flex"
+            backgroundColor={colors.primary[400]}
+            borderRadius="3px"
+        >
+            <InputBase sx={{ml: 2, flex: 1}} placeholder="Search"/>
+            <IconButton type="button" sx={{p: 1}}>
+                <SearchIcon/>
+            </IconButton>
+
+        </Box>
+
+        <IconButton sx={{display: "flex"}}></IconButton>
+    </Box>);
 }
 
 export default Topbar;
