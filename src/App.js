6b6843041d33ff3e3db07946eb8e9c66a04757f9
tri-nguyenducminh <tri.nguyenducminh@ncc.asia>
2023-01-31 17:37:06 +0700

    31/01/2023 1h02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar"
+import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import { Route, Routes } from "react-router-dom";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -9,9 +22,23 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app"> 
+        <div className="app">
+          <Sidebar/>
           <main className="content">
-            <Topbar />
+            <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+              {/* <Route path="/team" element={<Team/>}/> */}
+              {/* <Route path="/invoices" element={<Invoices/>}/> */}
+              {/* <Route path="/contacts" element={<Contacts/>}/> */}
+              {/* <Route path="/bar" element={<Bar/>}/> */}
+              {/* <Route path="/form" element={<Form/>}/> */}
+              {/* <Route path="/line" element={<Line/>}/> */}
+              {/* <Route path="/pie" element={<Pie/>}/> */}
+              {/* <Route path="/faq" element={<FAQ/>}/> */}
+              {/* <Route path="/geography" element={<Geography/>}/> */}
+              {/* <Route path="/calendar" element={<Calendar/>}/> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
