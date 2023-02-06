f5a79f868fd51fea5e04dbbb999761400b6358e9
tri-nguyenducminh <tri.nguyenducminh@ncc.asia>
2023-02-06 17:16:23 +0700

    06/02/2023 https://www.youtube.com/watch?v=H-LvaBNLDSQ&list=PLpwngcHZlPadhRwryAXw3mJWX5KH3T5L3&ab_channel=CodingNepal 1p37

---
+++ src/scenes/RandomColorPaletteGenerator/RandomColorPaletteGenerator.jsx
@@ -0,0 +1,15 @@
+const RandomColorPaletteGenerator = () => {
+    return <html lang="en" dir="ltr">
+    <head>
+        <meta charSet={"utf-8"}/>
+        <title>Color Palette Generator</title>
+        <link rel={"stylesheet"} href={"style.css"}/>
+        <meta name={"viewport"} content={"width=device-width, initial-scale=1.0"}/>
+    </head>
+    <body>
+
+    </body>
+    </html>
+}
+
+export default RandomColorPaletteGenerator;
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -20,14 +20,33 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 
 const Sidebar = () => {
-    const theme = useTheme O
+    const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [selected, setSelected] = useState("Dashboard");
 
-    return <div>
-        Sidebar
-    </div>
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: '%(colors.primary[400]} !important'
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-inner-item:active": {
+                    color: "#868dfa !important"
+                },
+            }}
+        >
+        </Box>
+    );
 }
 
 export default Sidebar;
